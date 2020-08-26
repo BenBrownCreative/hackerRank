0cb9dbe97b53064ff4912641594d075055bb5aca
Ben Brown <designwithbrown@gmail.com>
2020-08-26 11:39:32 -0500

    hacker ranck switch challenge

--- 10DaysOfJS/getLetter.js
+++ 10DaysOfJS/getLetter.js
@@ -1,8 +1,26 @@
 function getLetter(s) {
   let letter;
+  const setA = ['a', 'e', 'i', 'o', 'u'];
+  const setB = ['b', 'c', 'd', 'f', 'g'];
+  const setC = ['h', 'j', 'k', 'l', 'm'];
   // Write your code here
+  const firstLetter = s[0].toLowerCase();
+  switch (firstLetter) {
+    case setA.find((x) => x === firstLetter):
+      letter = 'A';
+      break;
+    case setB.find((x) => x === firstLetter):
+      letter = 'B';
+      break;
+    case setC.find((x) => x === firstLetter):
+      letter = 'C';
+      break;
+    default:
+      letter = 'D';
+      break;
+  }
 
   return letter;
 }
 
-console.log('getLetter', getLetter('adfgt'));
+console.log('getLetter', getLetter('xzfgt'));
